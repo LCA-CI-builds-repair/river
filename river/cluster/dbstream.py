1cafbee5bdeb9fa298b97c8e2819f53f4768d926
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:33:59 +0000

    test_lca_code_editing__code-editing-metrics-668819e8502524f7__id_97

--- river/cluster/dbstream.py
+++ river/cluster/dbstream.py
@@ -226,12 +226,11 @@ class DBSTREAM(base.Clusterer):
                             )
                             self.s_t[i][j] = self._time_stamp
                         except KeyError:
-                            try:
-                                self.s[i][j] = 0
-                                self.s_t[i][j] = self._time_stamp
-                            except KeyError:
-                                self.s[i] = {j: 0}
-                                self.s_t[i] = {j: self._time_stamp}
+                            self.s[i][j] = 0
+                            self.s_t[i][j] = self._time_stamp
+                        except KeyError:
+                            self.s[i] = {j: 0}
+                            self.s_t[i] = {j: self._time_stamp}
 
             # prevent collapsing clusters
             for i in neighbor_clusters.keys():
