862052508e9fce4f829ede241da95bae26faab5f
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:00:14 +0000

    test_lca_code_editing__code-editing-metrics-x688f9e9d9d296ff9__id_99

--- river/covariance/emp.py
+++ river/covariance/emp.py
@@ -209,7 +209,7 @@ class EmpiricalCovariance(SymmetricMatrix):
             except KeyError:
                 self._cov[i, j] = stats.Cov(self.ddof)
                 if isinstance(cov, dict):
-                    cov_ = cov.get((i, j), cov.get((j, i)))
+                    cov_ = cov.get((i, j), cov.get((j, i), 0))
                 else:
                     cov_ = cov
                 self._cov[i, j] += stats.Cov._from_state(
@@ -225,12 +225,12 @@ class EmpiricalCovariance(SymmetricMatrix):
                 self[i, i]
             except KeyError:
                 self._cov[i, i] = stats.Var(self.ddof)
-            if isinstance(cov, dict):
-                if isinstance(cov, dict):
-                    cov_ = cov[i, i]
-                else:
-                    cov_ = cov
-            self._cov[i, i] += stats.Var._from_state(n=n, m=mean[i], sig=cov_, ddof=self.ddof)
+            self._cov[i, i] += stats.Var._from_state(
+                n=n,
+                m=mean[i],
+                sig=cov.get((i, i), cov if isinstance(cov, (float, int)) else 0),
+                ddof=self.ddof
+            )
 
     @classmethod
     def _from_state(cls, n: int, mean: dict, cov: float | dict, *, ddof=1):
