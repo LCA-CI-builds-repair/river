6ddd7647361a654bfc5d46f4b8aafe79d74a1677
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 20:57:48 +0000

    test_lca_code_editing__code-editing-metrics-x2f618c002d7b875d__id_98

--- river/anomaly/sad.py
+++ river/anomaly/sad.py
@@ -9,6 +9,8 @@ class StandardAbsoluteDeviation(anomaly.base.AnomalyDetector):
     r"""Standard Absolute Deviation (SAD).
 
     SAD is the model that calculates the anomaly score by using the deviation from the mean/median, divided by the
+    standard deviation/mean absolute deviation.
+    """
     standard deviation of all the points seen within the data stream. The idea of this model is based on
     the $3 \times \sigma$ rule described in [^1].
 
