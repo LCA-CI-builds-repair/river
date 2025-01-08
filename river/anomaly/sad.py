925ee26405a10002d58f109761a34b4780da5f81
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-08 17:47:23 +0000

    waleko__gpt-4o_1de6__id_98

--- river/anomaly/sad.py
+++ river/anomaly/sad.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from river import anomaly, stats
+from typing import Union
 
 __all__ = ["StandardAbsoluteDeviation"]
 
@@ -59,6 +60,7 @@ class StandardAbsoluteDeviation(anomaly.base.AnomalyDetector):
     """
 
     def __init__(self, sub_stat: str = "mean"):
+        self.subtracted_statistic_estimator: Union[stats.Mean, stats.Quantile]
         self.variance = stats.Var()
         self.sub_stat = sub_stat
 
