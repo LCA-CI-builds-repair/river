7c19922fc173544b2b7407317dd77e315fe43bcb
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-16 16:11:23 +0000

    waleko__claude-3.5-sonnet_bbfd__id_97

--- river/covariance/emp.py
+++ river/covariance/emp.py
@@ -204,15 +204,22 @@ class EmpiricalCovariance(SymmetricMatrix):
             KeyError: If an element in `mean` or `cov` is missing.
         """
         for i, j in itertools.combinations(mean.keys(), r=2):
+            if isinstance(cov, dict):
+                cov_ = cov.get((i, j), cov.get((j, i)))
+            else:
+                cov_ = cov
+                
             try:
-                self[i, j]
+                self._cov[i, j] += stats.Cov._from_state(
+                    n=n,
+                    mean_x=mean[i],
+                    mean_y=mean[j],
+                    cov=cov_,
+                    ddof=self.ddof,
+                )
             except KeyError:
                 self._cov[i, j] = stats.Cov(self.ddof)
-                if isinstance(cov, dict):
-                    cov_ = cov.get((i, j), cov.get((j, i)))
-                else:
-                    cov_ = cov
-                self._cov[i, j] += stats.Cov._from_state(
+                self._cov[i, j] = stats.Cov._from_state(
                     n=n,
                     mean_x=mean[i],
                     mean_y=mean[j],
@@ -221,16 +228,26 @@ class EmpiricalCovariance(SymmetricMatrix):
                 )
 
         for i in mean.keys():
+            if isinstance(cov, dict):
+                cov_ = cov[i, i]
+            else:
+                cov_ = cov
+                
             try:
-                self[i, i]
+                self._cov[i, i] += stats.Var._from_state(
+                    n=n, 
+                    m=mean[i], 
+                    sig=cov_, 
+                    ddof=self.ddof
+                )
             except KeyError:
                 self._cov[i, i] = stats.Var(self.ddof)
-            if isinstance(cov, dict):
-                if isinstance(cov, dict):
-                    cov_ = cov[i, i]
-                else:
-                    cov_ = cov
-            self._cov[i, i] += stats.Var._from_state(n=n, m=mean[i], sig=cov_, ddof=self.ddof)
+                self._cov[i, i] = stats.Var._from_state(
+                    n=n, 
+                    m=mean[i], 
+                    sig=cov_, 
+                    ddof=self.ddof
+                )
 
     @classmethod
     def _from_state(cls, n: int, mean: dict, cov: float | dict, *, ddof=1):
