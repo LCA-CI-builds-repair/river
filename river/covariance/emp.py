744c7c461b98fe703d3301c1ce3a858e2099a2c0
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-16 01:46:07 +0000

    waleko__claude-3.5-sonnet_a930__id_99

--- river/covariance/emp.py
+++ river/covariance/emp.py
@@ -225,12 +225,11 @@ class EmpiricalCovariance(SymmetricMatrix):
                 self[i, i]
             except KeyError:
                 self._cov[i, i] = stats.Var(self.ddof)
-            if isinstance(cov, dict):
-                if isinstance(cov, dict):
-                    cov_ = cov[i, i]
-                else:
-                    cov_ = cov
-            self._cov[i, i] += stats.Var._from_state(n=n, m=mean[i], sig=cov_, ddof=self.ddof)
+            
+            cov_ = cov[i, i] if isinstance(cov, dict) else cov
+            self._cov[i, i] += stats.Var._from_state(
+                n=n, m=mean[i], sig=cov_, ddof=self.ddof
+            )
 
     @classmethod
     def _from_state(cls, n: int, mean: dict, cov: float | dict, *, ddof=1):
